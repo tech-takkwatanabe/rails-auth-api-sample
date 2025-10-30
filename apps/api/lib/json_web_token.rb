bd9f627ab5fdb0b1d2cf866d43f76f27b79c666e
tech-takkwatanabe <tech.takkwatanabe@gmail.com>
2025-10-30 22:39:47 +0900

    🎨 json_web_token.rb修正

--- apps/api/lib/json_web_token.rb
+++ apps/api/lib/json_web_token.rb
@@ -1,17 +1,19 @@
 require 'jwt'
 
 class JsonWebToken
-  SECRET_KEY = Rails.application.credentials.jwt_secret
-
   def self.encode(payload, exp = 24.hours.from_now)
     payload[:exp] = exp.to_i
-    JWT.encode(payload, SECRET_KEY)
+    JWT.encode(payload, secret_key)
   end
 
   def self.decode(token)
-    decoded = JWT.decode(token, SECRET_KEY)[0]
+    decoded = JWT.decode(token, secret_key)[0]
     HashWithIndifferentAccess.new decoded
   rescue JWT::DecodeError => e
     nil
+  end
+
+  def self.secret_key
+    ENV['JWT_SECRET_KEY']
   end
 end
