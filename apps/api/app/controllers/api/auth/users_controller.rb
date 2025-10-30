34c3bb9db3476bde09c6b1163784220a227c0fe5
tech-takkwatanabe <tech.takkwatanabe@gmail.com>
2025-10-30 22:41:48 +0900

    🎨 エンドポイントGET me追加

--- apps/api/app/controllers/api/auth/users_controller.rb
+++ apps/api/app/controllers/api/auth/users_controller.rb
@@ -1,6 +1,9 @@
 module Api
   module Auth
     class UsersController < ApplicationController
+      include Authenticable
+      skip_before_action :authenticate_request!, only: [:create]
+
       def create
         user = User.new(user_params)
         if user.save
@@ -8,6 +11,10 @@ module Api
         else
           render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
         end
+      end
+
+      def me
+        render json: { uuid: @current_user.uuid, name: @current_user.name, email: @current_user.email }, status: :ok
       end
 
       private
