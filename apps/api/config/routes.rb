e6ca97346092585ad65dca4f4b922e526a7c4d4d
tech-takkwatanabe <tech.takkwatanabe@gmail.com>
2025-10-30 22:40:20 +0900

    🎨 エンドポイント me追加

--- apps/api/config/routes.rb
+++ apps/api/config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
     namespace :auth do
       post :signup, to: 'users#create'
       post :login, to: 'sessions#create'
+      get :me, to: 'users#me'
     end
   end
 end
