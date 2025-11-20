b05dae3e507abf9d66ae2cba1be39df249ef94bd
tech-takkwatanabe <tech.takkwatanabe@gmail.com>
2025-11-20 22:29:23 +0900

    🎨 ログイン時にrefresh_token生成、redisに保管

--- apps/api/app/controllers/api/auth/sessions_controller.rb
+++ apps/api/app/controllers/api/auth/sessions_controller.rb
@@ -5,15 +5,17 @@ module Api
         user = User.find_by(email: params[:email])
 
         if user&.authenticate(params[:password])
-          # TODO: リフレッシュトークンの実装
           access_token = JsonWebToken.encode(user_id: user.id)
-          refresh_token = "dummy-refresh-token" # 仮実装
+          
+          # リフレッシュトークンの生成と保存
+          refresh_token = SecureRandom.hex(32)
+          $redis.set("refresh_token:#{refresh_token}", user.id, ex: 7.days.to_i)
 
           render json: { uuid: user.uuid, access_token: access_token, refresh_token: refresh_token }, status: :ok
         else
           render json: { error: 'Invalid email or password' }, status: :unauthorized
         end
-      end
+            end
     end
   end
 end
