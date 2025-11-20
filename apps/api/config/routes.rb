8d5fda0e362242eeded132b5fcbb64349f1307d3
tech-takkwatanabe <tech.takkwatanabe@gmail.com>
2025-11-20 22:28:26 +0900

    🎨 refresh用エンドポイント追加

--- apps/api/config/routes.rb
+++ apps/api/config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
       post :signup, to: 'users#create'
       post :login, to: 'sessions#create'
       get :me, to: 'users#me'
+      post :refresh, to: 'refreshes#create'
     end
   end
 end
